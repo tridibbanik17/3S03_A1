ffe9f2c07e09bbbb796e2e68fc6fd14fd2181b9c
Tridib Banik <tridib.perfect@gmail.com>
2026-02-16 19:09:12 -0500

    Program1 test cases

--- Q2/Program1/src/test/java/ca/mcmaster/sfwreng3s03/Program1Test.java
+++ Q2/Program1/src/test/java/ca/mcmaster/sfwreng3s03/Program1Test.java
@@ -1,25 +1,25 @@
 package ca.mcmaster.sfwreng3s03;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 class Program1Test {
 
+    // (b) Does NOT execute the fault
     @Test
     void valueFoundBeforeIndexZero() {
         int[] x = {2, 3, 5};
         assertEquals(2, A1_Q2_Program1_Incorrect.findLast(x, 5));
     }
 
+    // (c) Executes the fault but no error state
     @Test
     void elementAbsent() {
         int[] x = {2, 3, 5};
         assertEquals(-1, A1_Q2_Program1_Incorrect.findLast(x, 4));
     }
 
-    @Test
-    void correctResultDespiteFault() {
-        int[] x = {9, 3, 5};
-        assertEquals(2, A1_Q2_Program1_Incorrect.findLast(x, 5));
-    }
+    // (d) Not possible: any error state causes failure
+    //     because skipping index 0 changes the correct result.
+
 }
