da613cf0914e140e701633f7e9f74373f5650da9
Tridib Banik <tridib.perfect@gmail.com>
2026-02-26 23:32:17 -0500

    Fix test case for empty array in Program3Test

--- Q2/Program3/src/test/java/ca/mcmaster/sfwreng3s03/Program3Test.java
+++ Q2/Program3/src/test/java/ca/mcmaster/sfwreng3s03/Program3Test.java
@@ -5,18 +5,18 @@ import org.junit.jupiter.api.Test;
 
 class Program3Test {
 
-    // (b) Does NOT execute the fault (no zeros present)
-    @Test
-    void noZerosPresent() {
-        int[] x = {-3, 2, 5};
-        assertEquals(2, A1_Q2_Program3_Incorrect.countPositive(x));
-    }
-
-    // (c) Executes the fault but no error state (empty array)
+    // (b) Executes the fault but no error state (empty array)
     @Test
     void emptyArray() {
         int[] x = {};
         assertEquals(0, A1_Q2_Program3_Incorrect.countPositive(x));
+    }
+    
+    // (c) Does NOT execute the fault (no zeros present)
+    @Test
+    void noZerosPresent() {
+        int[] x = {-3, 2, 5};
+        assertEquals(2, A1_Q2_Program3_Incorrect.countPositive(x));
     }
 
     // (d) Not possible: any error state causes a failure
